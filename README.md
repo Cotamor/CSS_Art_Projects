bd9bbc10a3eca0e0f1c2c5b87dceff7e3948dbf9
Cotamor <cotamor@gmail.com>
2022-09-26 23:45:08 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CSS Art Project
+
+these projects are all learned from Coding Artist youtube channel [link](https://www.youtube.com/playlist?list=PLNCevxogE3fjcsmPpyz1Ho9VyatKyJlgU).
+
+watch it live [here]()</br>
---
+++ script.js
@@ -0,0 +1,32 @@
+const projects = [
+  { name: 'buildings_night_day' },
+  { name: 'lovely_dog' },
+  { name: 'night_sky' },
+  { name: 'panda' },
+  { name: 'pingpong' },
+  { name: 'sunset' },
+  { name: 'flying_bird' },
+]
+const list = document.getElementById('list')
+
+projects.forEach(({ name }, idx) => {
+  // console.log(name)
+  const listItem = document.createElement('li')
+  listItem.innerHTML = `
+    <a href="./${name}/index.html" >
+      <img src="./${name}/desktop-design.png" alt="${name}" />
+    </a>
+    <div class="content">
+      <h3>${idx + 1}. ${formatTitle(name)}</h3>
+      <a href="/${name}"><button>View Live</button></a>
+    </div>
+  `
+  list.appendChild(listItem)
+})
+
+function formatTitle(title) {
+  return title
+    .split('_')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ')
+}
